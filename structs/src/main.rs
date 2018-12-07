2fb7267db01a4283123363772ab0e534730cb67b
Leonardo Razovic <lrazovic@iMac-di-Leonardo.local>
2018-12-07 11:30:09 +0100

    More structs examples

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -12,14 +12,37 @@ fn build_user(name: String, surname: String) -> User {
     }
 }
 
+struct Rectangle {
+    height: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+    fn perimeter(&self) -> u32 {
+        (self.height + self.width)*2
+    }
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+}
+
+/* Old School, more code way
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.height * rectangle.width
+}
+*/
+
 fn main() {
     let name = String::from("Leonardo");
     let surname = String::from("Razovic");
     let mut user = build_user(name, surname);
-    println!("Name: {}", user.name);
-    println!("Surname: {}", user.surname);
-    println!("Active: {}", user.active);
-    println!("Deactivating...");
     user.active = false;
-    println!("Active: {}", user.active);
+    let figure = Rectangle {height: 30, width: 20};
+    let square = Rectangle::square(20);
+    println!("Area = {}", figure.area());
+    println!("Perimeter = {}", figure.perimeter());
+    println!("Area = {}", square.area());
 }
