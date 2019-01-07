15b3f43213dce9fde952472642e126da8c09a1b0
Leonardo Razovic <lrazovic@gmail.com>
2019-01-07 19:42:25 +0100

    Enums and Pattern Matching

---
+++ enumplay/src/main.rs
@@ -0,0 +1,17 @@
+enum IpAddrKind {
+    V4,
+    V6
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    println!("{}", home.address);
+}
