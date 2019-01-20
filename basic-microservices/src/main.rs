8e7909be2bb1ec8bc2ba10aece937ee660267938
Leonardo Razovic <lrazovic@gmail.com>
2019-01-20 12:28:37 +0100

    Basic Microservices starting point using hyper

---
+++ basic-microservices/src/main.rs
@@ -0,0 +1,40 @@
+extern crate futures;
+extern crate hyper;
+
+use futures::{future, Future};
+use hyper::service::service_fn;
+use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
+
+const INDEX: &'static str = r#"
+<!doctype html>
+<html>
+    <head>
+        <title>Rust Microservice</title>
+    </head>
+    <body>
+        <h3>Rust Microservice</h3>
+    </body>
+</html>
+"#;
+
+fn microservice_handler(req: Request<Body>) -> impl Future<Item = Response<Body>, Error = Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => future::ok(Response::new(INDEX.into())),
+        _ => {
+            let response = Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::empty())
+                .unwrap();
+            future::ok(response)
+        }
+    }
+}
+
+fn main() {
+    let addr = ([127, 0, 0, 1], 8080).into();
+    let builder = Server::bind(&addr);
+    let server = builder.serve(|| service_fn(microservice_handler));
+    let server = server.map_err(drop);
+    println!("Server running on {}:{}", addr.ip(), addr.port());
+    hyper::rt::run(server);
+}
