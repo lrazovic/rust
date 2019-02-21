be98f12af40f98b58256ba459226fbc60dd5169c
lrazovic <leorazzo@gmail.com>
2019-02-21 16:51:47 +0100

    simple process exit

--- simplegrep/src/main.rs
+++ simplegrep/src/main.rs
@@ -1,8 +1,12 @@
 use std::env;
-use std::fs;
+use std::process;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
+	if args.len() == 1 || args.len() == 2 {
+		eprintln!("USAGE: cargo run <query> <file>");
+		process::exit(-1)
+	}
 	let query = &args[1];
 	let file = &args[2];
 	print!("Searching {:?} ",query);
