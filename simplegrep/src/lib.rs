b925b39182547ab6489c8a27cefe1cd152effc29
lrazovic <leorazzo@gmail.com>
2019-02-27 12:53:01 +0100

    betetr error handling

--- simplegrep/src/lib.rs
+++ simplegrep/src/lib.rs
@@ -1,8 +1,28 @@
-#[cfg(test)]
-mod tests{
-    use super::*;
+use std::error::Error;
+use std::fs;
+use std::process;
 
-    fn name() {
-        unimplemented!();
-    }
+pub struct Config {
+	pub query: String,
+	pub file: String,
+}
+
+impl Config {
+	pub fn new(args: &[String]) -> Config {
+		if args.len() < 3 {
+			eprintln!("USAGE: cargo run <query> <file>");
+			process::exit(-1)
+		}
+		let query = args[1].clone();
+		let file = args[2].clone();
+		Config { query, file }
+	}
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+	let contents = fs::read_to_string(config.file)?;
+
+	println!("With text:\n{}", contents);
+
+	Ok(())
 }
--- simplegrep/src/main.rs
+++ simplegrep/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::fs;
 use std::process;
+use simplegrep::Config;
 
 /*
 1. Calling the command line parsing logic with the argument values
@@ -9,31 +9,13 @@ use std::process;
 4. Handling the error if run returns an error
 */
 
-struct Config {
-	query: String,
-	file: String,
-}
-
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	usage(&args);
-	let config = parse_config(&args);
+	let config = Config::new(&args);
 	print!("Searching {:?} ", config.query);
 	println!("in File {}", config.file);
-	let contents = fs::read_to_string(config.file).expect("Something went wrong reading the file");
-	println!("With text:\n{}", contents);
-}
-
-fn parse_config(args: &[String]) -> Config {
-	let query = args[1].clone();
-	let file = args[2].clone();
-	Config {query, file}
-}
-
-fn usage(args: &[String]) {
-	let len = args.len();
-	if len == 1 || len == 2 {
-		eprintln!("USAGE: cargo run <query> <file>");
+	if let Err(e) = simplegrep::run(config) {
+		println!("Application error: {}", e);
 		process::exit(-1)
 	}
 }
