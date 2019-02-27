f72fc62cae590c76035a377b10237522f70264cd
lrazovic <leorazzo@gmail.com>
2019-02-27 12:19:19 +0100

    simple refactoring

--- simplegrep/src/main.rs
+++ simplegrep/src/main.rs
@@ -2,16 +2,38 @@ use std::env;
 use std::fs;
 use std::process;
 
+/*
+1. Calling the command line parsing logic with the argument values
+2. Setting up any other configuration
+3. Calling a run function in lib.rs
+4. Handling the error if run returns an error
+*/
+
+struct Config {
+	query: String,
+	file: String,
+}
+
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	if args.len() == 1 || args.len() == 2 {
+	usage(&args);
+	let config = parse_config(&args);
+	print!("Searching {:?} ", config.query);
+	println!("in File {}", config.file);
+	let contents = fs::read_to_string(config.file).expect("Something went wrong reading the file");
+	println!("With text:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> Config {
+	let query = args[1].clone();
+	let file = args[2].clone();
+	Config {query, file}
+}
+
+fn usage(args: &[String]) {
+	let len = args.len();
+	if len == 1 || len == 2 {
 		eprintln!("USAGE: cargo run <query> <file>");
 		process::exit(-1)
 	}
-	let query = &args[1];
-	let file = &args[2];
-	let contents = fs::read_to_string(file).expect("Something went wrong reading the file");
-	print!("Searching {:?} ", query);
-	println!("in File {}", file);
-	println!("With text:\n{}", contents);
 }
