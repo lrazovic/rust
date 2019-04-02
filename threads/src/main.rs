80fa0da62394d290353e0ac4c1577925009f0203
lrazovic <leorazzo@gmail.com>
2019-04-02 09:46:44 +0200

    cleanup

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -2,6 +2,11 @@ use std::sync::mpsc; //multiple producer, single consumer
 use std::thread;
 use std::time::Duration;
 
+struct Audio {
+    len: i32,
+    name: String,
+}
+
 fn main() {
     let (tx, rx) = mpsc::channel();
     thread::spawn(move || {
@@ -17,9 +22,13 @@ fn main() {
             thread::sleep(Duration::from_secs(2));
         }
     });
-    
 
     for msg in rx {
         println!("Received {}", msg);
     }
+    let track = Audio {
+        len: 32,
+        name: String::from("Name"),
+    };
+    println!("id {} name {}", track.len, track.name);
 }
